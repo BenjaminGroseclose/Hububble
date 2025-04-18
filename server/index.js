dab0d76e933e4086ba93ec2bedd9f1da14d46f9c
Ben Groseclose <ben.groseclose01@gmail.com>
2025-04-17 21:01:39 -0400

    Change to init app

--- server/index.js
+++ server/index.js
@@ -1,85 +1,83 @@
 // Set up server
 const path = require("path");
 const express = require("express");
+const http = require("http");
 const morgan = require("morgan");
-const compression = require("compression")
+const compression = require("compression");
 const { createClient } = require("redis");
+const { Server } = require("socket.io");
+
 const PORT = process.env.PORT || 8080;
 const app = express();
-const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
 
 // Initialize Redis client
 const redisClient = createClient();
 
-redisClient.on('error', (err) => console.error('Redis Client Error', err));
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
 (async () => {
   await redisClient.connect();
-  console.log('Connected to Redis');
+  console.log("Connected to Redis");
 })();
 
 module.exports = { app, redisClient };
 
 const createApp = () => {
-    //Set up logging middleware
-    app.use(morgan("dev"));
+  //Set up logging middleware
+  app.use(morgan("dev"));
 
-    // Set up body parsing middleware
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
-    
-    // Set up compression middleware
-    app.use(compression());
+  // Set up body parsing middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-    // Set up static file-serving middleware
-    app.use(express.static(path.join(__dirname, "..", "public")));
+  // Set up compression middleware
+  app.use(compression());
 
-    // Send 404 for unrecongnized request
-    app.use((req, res, next) =>{
-        if (path.extname(req.path, next).length) {
-            const err = new Error("Not found")
-            err.status = 404;
-            next(err)
-        } 
-        else 
-        {
-            next();
-        }
-    });
+  // Set up static file-serving middleware
+  app.use(express.static(path.join(__dirname, "..", "public")));
 
-    //Send index.html
-    app.use("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "..", "public/index.html"))
-    });
+  // Send 404 for unrecongnized request
+  app.use((req, res, next) => {
+    if (path.extname(req.path, next).length) {
+      const err = new Error("Not found");
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
 
-    // Error handling endware
-    app.use((err, req, res, next) => 
-    {
-        console.error(err);
-        console.error(err.stack);
-        res.status(err.status || 500).send(err.message || "Internal server error.");
-    });
+  //Send index.html
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public/index.html"));
+  });
 
-    
+  // Error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
 };
 
 const startListening = () => {
-    // Create a server object
-    const server = app.listen(PORT, () => 
-        console.log(`Listening on port ${PORT}`)
-    );
-    const io = socketio(server);
-    require("./socket/index")(io, redisClient); // Explicitly specify the correct file path
+  // Create a server object
+  const server = app.listen(PORT, () =>
+    console.log(`Listening on port ${PORT}`)
+  );
+
+  require("./socket/index")(io, redisClient); // Explicitly specify the correct file path
 };
 
 async function bootApp() {
-    await createApp();
-    await startListening();
+  await createApp();
+  await startListening();
 }
 
 if (require.main === module) {
-    bootApp();
-}
-else{
-    createApp();
+  bootApp();
+} else {
+  createApp();
 }
